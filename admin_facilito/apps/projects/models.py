3389727a8408075fe34a519d19d5dfce18d99b7f
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-12 15:57:39 -0600

    Creando modelo, migración y registrandolo en el admin

--- admin_facilito/apps/projects/models.py
+++ admin_facilito/apps/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import datetime
+
+
+class Project(models.Model):
+    """ Project model """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    dead_line = models.DateField()
+    created = models.DateField(default=datetime.date.today)
+
+
+    def __str__(self):
+        return self.title
+    
