c94eed833f20b3f433ab4d4449af93c50c1ae40c
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-13 16:36:45 -0600

    Pequeño refactor en las urls

--- admin_facilito/apps/projects/urls.py
+++ admin_facilito/apps/projects/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from .views import CreateProjectView, ListProjectsView, ShowProjectView
 from .views import edit_project
+from .views import CreateProjectView, ListProjectsView, ShowProjectView
 
 urlpatterns = [
-    url(r'^create/$', CreateProjectView.as_view(), name='create'),
-    url(r'^projects/$', ListProjectsView.as_view(), name='my_projects'),
-    url(r'^edit/(?P<slug>[\w-]+)/$', edit_project, name='edit'),
+    url(r'^mine/$', ListProjectsView.as_view(), name='my_projects'),
     url(r'^show/(?P<slug>[\w-]+)/$', ShowProjectView.as_view(), name='show'),
+    url(r'^create/$', CreateProjectView.as_view(), name='create'),
+    url(r'^edit/(?P<slug>[\w-]+)/$', edit_project, name='edit'),
 ]
